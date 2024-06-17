b5f7c3894c6f26f70098ebf87757ce22cde9aa36
duchnv-vn <huynhnguyenvietduc@gmail.com>
2024-06-17 19:44:03 +0700

    Exercism: armstrong-numbers

--- src/exercism/lua/armstrong-numbers/armstrong-numbers.lua
+++ src/exercism/lua/armstrong-numbers/armstrong-numbers.lua
@@ -1,6 +1,19 @@
 local ArmstrongNumbers = {}
 
 function ArmstrongNumbers.is_armstrong_number(number)
+    local numberStr = tostring(number)
+    local power_number = #numberStr
+    if power_number == 1 then return true end
+
+    local armsstrong_number = 0
+    for i = 1, power_number do
+        local number_unit = numberStr:sub(i, i)
+        armsstrong_number = armsstrong_number + (tonumber(number_unit) ^ power_number)
+    end
+
+    return armsstrong_number == number
 end
+
+ArmstrongNumbers.is_armstrong_number(153)
 
 return ArmstrongNumbers
